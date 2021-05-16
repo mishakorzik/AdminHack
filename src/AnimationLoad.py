f030aa3f402ac1514f201836d2380ece7eb66ff4
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-16 22:09:44 +0300

    Create AnimationLoad.py

---
+++ src/AnimationLoad.py
@@ -0,0 +1,22 @@
+import time
+import itertools
+import threading
+import sys
+
+done = False
+
+def animate():
+    for c in itertools.cycle(['|', '/', '—', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rWAIT A MOMENT ADMINHACK LOADING ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('')
+
+t = threading.Thread(target=animate)
+t.start()
+
+ 
+time.sleep(3)
+done = True

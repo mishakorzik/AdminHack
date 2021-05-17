f02b7a1405cebeb62c50666ebc1c87af73639d0f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-17 20:09:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 clear
-echo By mishakorzhik
 sleep 1
 echo Installing please wait...
 sleep 2
-pkg install toilet
 pkg install python2
+pkg install python3
 pkg install php
 sleep 1 
-toilet -F gay Done
-
+echo ██████╗░░█████╗░███╗░░██╗███████╗
+echo ██╔══██╗██╔══██╗████╗░██║██╔════╝
+echo ██║░░██║██║░░██║██╔██╗██║█████╗░░
+echo ██║░░██║██║░░██║██║╚████║██╔══╝░░
+echo ██████╔╝╚█████╔╝██║░╚███║███████╗
+echo ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
+echo         Developer: mishakorzhik
+echo          Update on: 16 05 2021
